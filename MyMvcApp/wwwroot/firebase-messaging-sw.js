71d2950157dc1803ed15958b47dceb9240e0ff86
Sanjay <Sanjay@DESKTOP-DNCGI3S>
2024-12-05 01:42:18 +0530

    Add project files.

---
+++ MyMvcApp/wwwroot/firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+﻿importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCbyEcFoAjlnKBG8P_UG4aqLVWzyRDPg0o",
+    authDomain: "demoapp-fee05.firebaseapp.com",
+    projectId: "demoapp-fee05",
+    storageBucket: "demoapp-fee05.firebasestorage.app",
+    messagingSenderId: "425154052473",
+    appId: "1:425154052473:web:213f7e32e199cca14d6fa8"  
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+        '[firebase-messaging-sw.js] Received background message ',
+        payload
+    );
+    // Customize notification here
+    //const notificationTitle = 'Background Message Title';
+    //const notificationOptions = {
+    //    body: 'Background Message body.',
+    //    icon: '/firebase-logo.png'
+    //};
+
+    //self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
